b134a5dd17d7d6e42a5b0c0b94574208dd9a5051
Puleng Veronica <222914556@mycput.ac.za>
2025-08-31 18:15:28 +0200

    basr

---
+++ src/main/java/za/ac/cput/domain/PaymentStatus.java
@@ -0,0 +1,12 @@
+package za.ac.cput.domain;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+
+@JsonFormat(shape = JsonFormat.Shape.STRING)
+public enum PaymentStatus {
+    PENDING,
+    COMPLETED,
+    FAILED,
+    CANCELLED,
+    REFUNDED
+}
